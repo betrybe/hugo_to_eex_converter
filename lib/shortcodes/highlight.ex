3476982b621ea0687e65a80c9171ded8c595f464
Norberto Oliveira Junior <norbejunior@gmail.com>
2020-05-22 15:15:22 -0300

    Implements Shortcode.Highlight converter

---
+++ lib/shortcodes/highlight.ex
@@ -0,0 +1,23 @@
+defmodule HugoToEexConverter.Shortcodes.Highlight do
+  @regex ~r/\s*?{{<\s?highlight (\w+)?\s?([^>\s]+)? ?>}}\n?(.+?)\n(?:\s*?{{<\s?\/\s?highlight\s?>}})/s
+
+  def convert(file_content) do
+    Regex.replace(@regex, file_content, &do_convert/4)
+  end
+
+  defp do_convert(_, "", "\"linenos=inline\"", inner_content) do
+    "\n\n```\n#{inner_content}\n```\n{: .line-numbers}"
+  end
+
+  defp do_convert(_, "", "", inner_content) do
+    "\n\n```\n#{inner_content}\n```\n"
+  end
+
+  defp do_convert(_, lang, "\"linenos=inline\"", inner_content) do
+    "\n\n```language-#{lang}\n#{inner_content}\n```\n{: .line-numbers}"
+  end
+
+  defp do_convert(_, lang, _g2, inner_content) do
+    "\n\n```language-#{lang}\n#{inner_content}\n```"
+  end
+end
---
+++ test/shortcodes/highlight_test.exs
@@ -0,0 +1,107 @@
+defmodule HugoToEexConverter.Shortcodes.HighlightTest do
+  use ExUnit.Case, async: true
+
+  alias HugoToEexConverter.Shortcodes
+
+  describe "convert/1" do
+    test "converts to a specific language highlight" do
+      content = """
+      Para entendermos melhor, vamos utilizar o seguinte código como exemplo:
+
+      {{< highlight javascript >}}
+      const multiply = (a, b) => a * b;
+      {{< /highlight >}}
+
+      Primeiro vamos entender o que o código acima faz, linha a linha:
+      """
+
+      content = Shortcodes.Highlight.convert(content)
+
+      assert content ==
+               ~s"""
+               Para entendermos melhor, vamos utilizar o seguinte código como exemplo:
+
+               ```language-javascript
+               const multiply = (a, b) => a * b;
+               ```
+
+               Primeiro vamos entender o que o código acima faz, linha a linha:
+               """
+    end
+
+    test "converts to a specific language highlight with line numbers" do
+      content = """
+      {{< highlight javascript "linenos=inline" >}}
+      const expires = moment().add(1, 'minutes').valueOf();
+      {{< /highlight >}}
+      """
+
+      content = Shortcodes.Highlight.convert(content)
+
+      assert content ==
+               ~s"""
+               \n\n```language-javascript
+               const expires = moment().add(1, 'minutes').valueOf();
+               ```
+               {: .line-numbers}
+               """
+    end
+
+    test "converts to a non-specific language highlight" do
+      content = """
+      {{< highlight >}}
+      O nome do curso é: Curso de desenvolvimento Front-end 2019/09 - BH
+      {{< /highlight >}}
+      """
+
+      content = Shortcodes.Highlight.convert(content)
+
+      assert content ==
+               """
+               \n\n```
+               O nome do curso é: Curso de desenvolvimento Front-end 2019/09 - BH
+               ```\n
+               """
+    end
+
+    test "converts to a non-specific language highlight with line numbers" do
+      content = """
+      {{< highlight "linenos=inline" >}}
+      O nome do curso é: Curso de desenvolvimento Front-end 2019/09 - BH
+      {{< /highlight >}}
+      """
+
+      content = Shortcodes.Highlight.convert(content)
+
+      assert content ==
+               """
+               \n\n```
+               O nome do curso é: Curso de desenvolvimento Front-end 2019/09 - BH
+               ```
+               {: .line-numbers}
+               """
+    end
+
+    test "handles shortcodes with or without white spaces on open/close parts" do
+      content = """
+      Para entendermos melhor, vamos utilizar o seguinte código como exemplo:
+
+      {{<highlight "linenos=inline">}}
+      O nome do curso é: Curso de desenvolvimento Front-end 2019/09 - BH
+      {{</highlight >}}
+      """
+
+      content = Shortcodes.Highlight.convert(content)
+
+      assert content ==
+               """
+               Para entendermos melhor, vamos utilizar o seguinte código como exemplo:
+
+               ```
+               O nome do curso é: Curso de desenvolvimento Front-end 2019/09 - BH
+               ```
+               {: .line-numbers}
+               """
+    end
+  end
+end
