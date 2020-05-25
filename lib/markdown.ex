ed3abf44a58775a66f8fbf2d937a6a30a307065e
Norberto Oliveira Junior <norbejunior@gmail.com>
2020-05-25 19:45:26 -0300

    Remove regex char in Markdown module

--- lib/markdown.ex
+++ lib/markdown.ex
@@ -1,6 +1,6 @@
 defmodule HugoToEExConverter.Markdown do
   def replace_italic_underscore_syntax_to_asterisc(content) do
-    Regex.replace(~r/\s_([\w-ú]+)_([\s\.!?])?/u, content, " *\\g{1}*\\g{2}")
+    Regex.replace(~r/\s_([\w-]+)_([\s\.!?])?/u, content, " *\\g{1}*\\g{2}")
   end
 
   def escape_forward_slashs_after_http_as_param(content) do
