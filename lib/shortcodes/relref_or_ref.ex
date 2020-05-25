4bea9f75374e4f6e0a309c624687b599730bc2b8
Norberto Oliveira Junior <norbejunior@gmail.com>
2020-05-25 18:56:58 -0300

    Implements Shortcodes.RelrefOrRef

---
+++ lib/shortcodes/relref_or_ref.ex
@@ -0,0 +1,22 @@
+defmodule HugoToEexConverter.Shortcodes.RelrefOrRef do
+  @content_dir "../trybe/priv/markdown_templates/content/"
+
+  def handle_path(path, file_path) do
+    path = Regex.replace(~r/[\\""]/, path, "")
+
+    do_handle_path(path, file_path)
+  end
+
+  def do_handle_path("/" <> path, _), do: "/course/#{path}"
+
+  def do_handle_path("#" <> _ = anchor, _), do: anchor
+
+  def do_handle_path(path, file_path) do
+    relative_path =
+      file_path
+      |> Path.dirname()
+      |> Path.relative_to(@content_dir)
+
+    "/#{relative_path}/#{path}"
+  end
+end
---
+++ test/shortcodes/relref_or_ref_test.exs
@@ -0,0 +1,22 @@
+defmodule HugoToEexConverter.Shortcodes.RelrefOrRefTest do
+  use ExUnit.Case, async: true
+
+  alias HugoToEexConverter.Shortcodes
+
+  @file_path "../trybe/priv/markdown_templates/content/course/front-end/react/_index.md"
+
+  describe "handle_path/2" do
+    test "converts to a path relative to the current file folder" do
+      assert Shortcodes.RelrefOrRef.handle_path("introduction", @file_path) ==
+               "/course/front-end/react/introduction"
+    end
+
+    test "converts to a path relative to `/course` content folder" do
+      assert Shortcodes.RelrefOrRef.handle_path("/front-end", @file_path) == "/course/front-end"
+    end
+
+    test "converts to an anchor linking to the same page" do
+      assert Shortcodes.RelrefOrRef.handle_path("#sd-cohort-1", @file_path) == "#sd-cohort-1"
+    end
+  end
+end
