bb524e5c299a9e09e6490693fc133c1271a84254
Norberto Oliveira Junior <norbejunior@gmail.com>
2020-05-25 18:58:26 -0300

    Implements Shortcode.WithBlock converter

---
+++ lib/shortcodes/with_block.ex
@@ -0,0 +1,18 @@
+defmodule HugoToEexConverter.Shortcodes.WithBlock do
+  alias HugoToEexConverter.Shortcodes
+
+  @regex ~r/{{< ?(.+?) ([^>]+) ?>}}\n(.+?)\n(?:{{< ?\/ ?(.+?) ?>}})/s
+
+  def convert(file_content) do
+    file_content = Regex.replace(@regex, file_content, &do_convert/4)
+
+    Regex.replace(~r/, +?do %>/, file_content, " do %>")
+  end
+
+  defp do_convert(_, shortcode, params, details) do
+    params = Shortcodes.Attrs.convert(params)
+    new_shortcode = HugoToEexConverter.shortcodes() |> Map.get(shortcode)
+
+    "<%= #{new_shortcode}(%{#{params}}) do %>\n#{details}\n<% end %>"
+  end
+end
---
+++ test/shortcodes/with_block_test.exs
@@ -0,0 +1,26 @@
+defmodule HugoToEexConverter.Shortcodes.WithBlockTest do
+  use ExUnit.Case, async: true
+
+  alias HugoToEexConverter.Shortcodes
+
+  describe "convert/1" do
+    test "converts block shortcodes to eex do..end" do
+      file_content = """
+      {{< who-am-i name: "Norberto Oliveira Junior (Norba)", role: "Software Developer" >}}
+
+      Mineirin de Itararé
+
+      {{< /who-am-i >}}
+      """
+
+      assert Shortcodes.WithBlock.convert(file_content) ==
+               """
+               <%= whoami(%{name: "Norberto Oliveira Junior (Norba)", role: "Software Developer" }) do %>
+
+               Mineirin de Itararé
+
+               <% end %>
+               """
+    end
+  end
+end
