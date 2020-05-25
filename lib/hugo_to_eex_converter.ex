ec1ead262b8ab612f52f50867cd9317a6955da46
Norberto Oliveira Junior <norbejunior@gmail.com>
2020-05-25 19:38:04 -0300

    Implements HugoToEexConverter module

--- lib/hugo_to_eex_converter.ex
+++ lib/hugo_to_eex_converter.ex
@@ -1,18 +1,39 @@
-defmodule ContentTranspiler do
-  @moduledoc """
-  Documentation for `ContentTranspiler`.
-  """
+defmodule HugoToEexConverter do
+  alias HugoToEexConverter.{Markdown, Shortcodes}
 
-  @doc """
-  Hello world.
+  def content_dir do
+    "tmp/content"
+  end
 
-  ## Examples
+  def convert(file_path) do
+    regex = ~r/^(?<frontmatter>(---\n(.*?)---)?)(\n*)(?<content>(.*))/ms
 
-      iex> ContentTranspiler.hello()
-      :world
+    %{
+      "frontmatter" => frontmatter,
+      "content" => content
+    } = Regex.named_captures(regex, File.read!(file_path))
 
-  """
-  def hello do
-    :world
+    [file_name, _] = String.split(file_path, ~r/.md$/)
+    file_name = Regex.replace(~r/_(index)$/, file_name, "\\g{1}")
+
+    create_frontmatter!(file_name, frontmatter)
+
+    File.write!("#{file_name}.html.md", do_convert(content, file_path))
+    File.rm!(file_path)
+  end
+
+  def create_frontmatter!(_path, ""), do: nil
+
+  def create_frontmatter!(path, content) do
+    File.write!(path <> ".yaml", content)
+  end
+
+  defp do_convert("", _), do: ""
+
+  defp do_convert(content, path) do
+    content
+    |> Shortcodes.convert(path)
+    |> Markdown.escape_forward_slashs_after_http_as_param()
+    |> Markdown.replace_italic_underscore_syntax_to_asterisc()
   end
 end
---
+++ test/hugo_to_eex_converter_test.exs
@@ -0,0 +1,136 @@
+defmodule HugoToEexConverterTest do
+  use ExUnit.Case, async: true
+  # doctest HugoToEexConverter
+
+  @fixture_file_path "test/support/content_fixture.md"
+  @hugo_markdown_file_path "test/support/course/module/subject/content.md"
+  @eex_markdown_file_path "test/support/course/module/subject/content.html.md"
+  @frontmatter_file_path "test/support/course/module/subject/content.yaml"
+
+  setup do
+    file_content = File.read!(@fixture_file_path)
+
+    File.write!(@hugo_markdown_file_path, file_content)
+  end
+
+  describe "convert/1" do
+    test "converts a hugo (go) template file to an eex (elixir)" do
+      HugoToEexConverter.convert(@hugo_markdown_file_path)
+
+      assert File.read!(@frontmatter_file_path) == expected_frontmatter()
+      assert File.read!(@eex_markdown_file_path) == expected_eex_markdown()
+    end
+  end
+
+  defp expected_frontmatter do
+    """
+    ---
+    title: "CSS Flexbox - Parte 1"
+    date: 2019-09-25
+    weight: 35
+    week: 6
+    day: 3
+    ---\
+    """
+  end
+
+  defp expected_eex_markdown do
+    ~S"""
+    <%= figure(%{src: "https:\/\/media.giphy.com/media/MTdHOLBVYpVOLY4gRN/giphy.gif", caption: "O que o banco pensa quando você diz \`JOIN\`", class: "text-center rounded mx-auto d-block", width: "588px", height: "auto"}) %>
+
+    Recapitulando o que aprendemos no módulo sobre [git](/course/intro/git), ao versionar nossos projetos, nós os associamos a repositórios remotos (_remotes_). Por padrão, adicionamos um *remote* chamado **origem**.
+
+    Um Flex container é criado ao usar a propriedade `display` de um elemento com o valor `flex`:
+
+    ```language-css
+    .flex-container {
+      display: flex;
+    }
+    ```
+
+    ---
+
+    <%= figure(%{src: "/course/module/images/css_flexbox_axes.png", class: "rounded mx-auto d-block", caption: "CSS Flexbox Axes", width: "600px", height: "auto", alt: "CSS Flexbox axes description"}) %>
+
+    [Turma 3 - Início jan/2020](/test/support/course/module/subject/sd-cohort-3)
+
+    [Turma 5 - Início jan/2020](#sd-cohort-5)
+
+    #### Flex Lines
+
+    <%= figure(%{src: "/course/module/subject/images/css_flexbox_single_line.png", class: "rounded mx-auto d-block", caption: "CSS Flexbox Single Line", width: "600px", height: "auto"}) %>
+
+    <%= figure(%{src: "/course/module/subject/module/images/css_flexbox_multi_line.png", class: "rounded mx-auto d-block", caption: "CSS Flexbox Multi Line", width: "600px", height: "auto"}) %>
+
+    Agora vamos ver a estrutura e propriedades do Flexbox.
+
+    ### Introdução e estrutura básica
+
+    <%= youtube_video "22PTowIiZUA" %>
+
+    ### Propriedades: flex-direction, flex-wrap, flex-flow e reverse
+
+    <%= youtube_video "ILHpH39QiG0" %>
+
+    * Teste as possibilidades (`row`, `column`, `row-reverse` e `column-reverse`) para a propriedade `flex-direction` [aqui](https://www.w3schools.com/css/tryit.asp?filename=trycss3_flexbox_flex-direction_column) {: .external-link target="_blank" rel="noreferrer noopener" }
+
+    * Teste as possibilidades (`wrap` e `nowrap`) para a propriedade `flex-wrap` [aqui](https://www.w3schools.com/css/tryit.asp?filename=trycss3_flexbox_flex-wrap_wrap) {: .external-link target="_blank" rel="noreferrer noopener" }
+
+    * Teste as possibilidades para a propriedade `flex-flow` [aqui](https://www.w3schools.com/css/tryit.asp?filename=trycss3_flexbox_flex-flow_row_wrap) {: .external-link target="_blank" rel="noreferrer noopener" }
+
+    ### Propriedades: justify-content
+
+    <%= youtube_video "V562GPPegJ4" %>
+
+    * Teste as possibilidades (`center`, `flex-start`, `flex-end`, `space-around` e `space-between`) para a propriedade `justify-content` [aqui](https://www.w3schools.com/css/tryit.asp?filename=trycss3_flexbox_justify-content_center) {: .external-link target="_blank" rel="noreferrer noopener" }
+
+    ### Propriedades: align-items
+
+    <%= youtube_video "TAuwOBN_F28" %>
+
+    * Teste as possibilidades (`center`, `flex-start`, `flex-end`, `stretch` e `baseline`) para a propriedade `align-items` [aqui](https://www.w3schools.com/css/tryit.asp?filename=trycss3_flexbox_align-items_baseline) {: .external-link target="_blank" rel="noreferrer noopener" }
+
+    ### Propriedades: align-content
+
+    <%= youtube_video "BVZ3AeYaGh0" %>
+
+    * Teste as possibilidades (`center`, `flex-start`, `flex-end`, `stretch`, `space-around` e `space-between`) para a propriedade `align-content` [aqui](https://www.w3schools.com/css/tryit.asp?filename=trycss3_flexbox_align-content_space-between) {: .external-link target="_blank" rel="noreferrer noopener" }
+
+    ---
+
+    ### Agora, a prática
+
+    Para os exercícios a seguir, utilize este [playground](https://the-echoplex.net/flexyboxes/) {: .external-link target="_blank" rel="noreferrer noopener" } para testar como as propriedades **Flexbox** influenciam a disposição dos elementos em um *container*. Existem outros *playgrounds* para **Flexbox** como este, e os links para eles estão nos **Recursos adicionais**.
+
+    Como os exercícios serão feitos na própria plataforma, crie um diretório em seu repositório para o site número 1 (problema do sapo) e outra para o site número 2 (problema das torres).
+    Salve o ***CSS*** que você criar na plataforma colocando o número do exercício como nome do arquivo, veja exemplos abaixo:
+
+    - Para o exercício número 3 do site número 1, o ***CSS*** deve ser salvo em `exercises/6.3/1/3.css`;
+
+    - Para o exercício número 8 do site número 2, o ***CSS*** deve ser salvo em `exercises/6.3/2/8.css`;
+
+    - Para o exercício número 9 do site número 2, o ***CSS*** deve ser salvo em `exercises/6.3/2/9.css`.
+
+    1. Faça os exercícios de **1** a **13** neste [site](https://flexboxfroggy.com/) {: .external-link target="_blank" rel="noreferrer noopener" }. O seu objetivo é colocar os sapos em cima de suas respectivas folhas.
+    2. Neste segundo [link](http://www.flexboxdefense.com/) {: .external-link target="_blank" rel="noreferrer noopener" }, complete os exercícios de **1** a **9**. Seu objetivo aqui é posicionar as torres de defesa para que elas abatam os inimigos. Você terá de pensar qual é a melhor posição para cada torre.
+
+    ---
+
+    ## Recursos adicionais (opcional)
+
+    * [W3Schools CSS Flexbox](https://www.w3schools.com/css/css3_flexbox.asp) {: .external-link target="_blank" rel="noreferrer noopener" }
+
+    * [CSS Tricks: A Complete Guide To Flexbox](https://css-tricks.com/snippets/css/a-guide-to-flexbox/) {: .external-link target="_blank" rel="noreferrer noopener" }
+
+    * [Playground: Properties for the flex container](https://codepen.io/enxaneta/full/adLPwv) {: .external-link target="_blank" rel="noreferrer noopener" }
+
+    * [Playground: Scotch.io](https://demos.scotch.io/visual-guide-to-css3-flexbox-flexbox-playground/demos/) {: .external-link target="_blank" rel="noreferrer noopener" }
+
+    ---
+
+    <%= versioning_your_code() %>
+
+    <%= next_button() %>
+    """
+  end
+end
