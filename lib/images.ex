5ff9d0913f668bdc692e099364aa783cae1049b6
Norberto Oliveira Junior <norbejunior@gmail.com>
2020-05-28 09:37:29 -0300

    Add test for HugoToEExConverter.Images

--- lib/images.ex
+++ lib/images.ex
@@ -1,15 +1,9 @@
 defmodule HugoToEExConverter.Images do
-  @glob "tmp/convert/content/**/*.{png,jpeg,jpg,gif,svg}"
-
-  def copy(glob \\ @glob) do
+  def copy(glob \\ "tmp/convert/content/**/*.{png,jpeg,jpg,gif,svg}") do
     glob
     |> Path.wildcard()
     |> Enum.map(&set_new_img_path/1)
     |> Enum.each(&do_copy/1)
-  end
-
-  defp set_new_img_path(img_path) do
-    [img_path, String.replace(img_path, "/convert/content/", "/converted/course/")]
   end
 
   defp do_copy([img_path, new_img_path]) do
@@ -19,5 +13,9 @@ defmodule HugoToEExConverter.Images do
     with :ok <- File.mkdir_p(dir) do
       File.write!(new_img_path, content)
     end
+  end
+
+  defp set_new_img_path(img_path) do
+    [img_path, String.replace(img_path, "/convert/content/", "/converted/course/")]
   end
 end
---
+++ test/images_test.exs
@@ -0,0 +1,19 @@
+defmodule HugoToEExConverter.ImagesTest do
+  use ExUnit.Case, async: true
+
+  alias HugoToEExConverter.Images
+
+  @img_path "test/support/converted/course/module/subject/trybe.svg"
+
+  setup do
+    on_exit(fn -> File.rm!(@img_path) end)
+  end
+
+  describe "copy/1" do
+    test "copies images from a folder to another" do
+      Images.copy("test/support/convert/**/*.{png,jpeg,jpg,gif,svg}")
+
+      assert File.exists?(@img_path)
+    end
+  end
+end
