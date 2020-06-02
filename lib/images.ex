170e43ec89f85f20c522353ed129151bdfe45b43
Norberto Oliveira Junior <norbejunior@gmail.com>
2020-06-02 16:23:14 -0300

    Fix path to files

--- lib/images.ex
+++ lib/images.ex
@@ -1,5 +1,5 @@
 defmodule HugoToEExConverter.Images do
-  @glob ~s|#{System.get_env("INPUT_STORAGE_PATH")}/content/**/*.{png,jpeg,jpg,gif,svg}|
+  @glob ~s|#{System.get_env("INPUT_STORAGEPATH")}/content/**/*.{png,jpeg,jpg,gif,svg}|
 
   def copy(glob \\ @glob) do
     glob
