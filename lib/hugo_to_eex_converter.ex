7b245e24aa4876e1367df6433cdd54c3c3c562d9
Norberto Oliveira Junior <norbejunior@gmail.com>
2020-06-02 14:28:46 -0300

    Fix path

--- lib/hugo_to_eex_converter.ex
+++ lib/hugo_to_eex_converter.ex
@@ -1,7 +1,7 @@
 defmodule HugoToEExConverter do
   alias HugoToEExConverter.{Markdown, Shortcodes}
 
-  @glob ~s|#{System.get_env("STORAGE_PATH")}/tmp/convert/**/*.md|
+  @glob ~s|#{System.get_env("INPUT_STORAGE_PATH")}/convert/**/*.md|
 
   def convert(glob \\ @glob) do
     glob
