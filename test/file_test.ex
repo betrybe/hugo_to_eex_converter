a4e88befa7afc3d74f0b07a93d0d415e73b4e143
Norberto Oliveira Junior <norbejunior@gmail.com>
2020-06-07 08:31:09 -0300

    Melhora a cobertura de teste de HugoToEExConverter.File

--- test/file_test.ex
+++ test/file_test.ex
@@ -3,21 +3,48 @@ defmodule HugoToEExConverter.FileTest do
 
   @file_path "test/support/converted/content/react/content"
 
+  @yaml_content
+  """
+  ---
+  title: "CSS Flexbox - Parte 1"
+  date: 2019-09-25
+  weight: 35
+  week: 6
+  day: 3
+  ---
+  """
+
+  @file_content
+  """
+  <%= figure(%{src: "//module/images/css_flexbox_axes.png", class: "rounded mx-auto d-block", caption: "CSS Flexbox Axes", width: "600px", height: "auto", alt: "CSS Flexbox axes description"}) %>
+
+  [Turma 3 - Início jan/2020](/content/module/subject/sd-cohort-3)
+
+  [Turma 5 - Início jan/2020](#sd-cohort-5)
+  """
+
+  setup do
+    on_exit(fn ->
+      File.rm!("#{@file_path}.yaml")
+      File.rm!("#{@file_path}.html.md")
+    end)
+  end
+
   describe "write/3" do
     test "creates an yaml file and append a flag into its content" do
       HugoToEExConverter.File.write(yaml_file_content, @file_path, ".yaml")
 
-      assert File.exists?("#{@file_path}.yaml")
+      assert File.read!("#{@file_path}.yaml") == expected_yaml_file_content()
     end
 
     test "creates an html.md file" do
       HugoToEExConverter.File.write(yaml_file_content, @file_path, ".html.md")
 
-      assert File.exists?("#{@file_path}.html.md")
+      assert File.read!("#{@file_path}.html.md") == @file_content
     end
   end
 
-  defp yaml_file_content do
+  defp expected_yaml_file_content do
     """
     ---
     title: "CSS Flexbox - Parte 1"
@@ -25,17 +52,8 @@ defmodule HugoToEExConverter.FileTest do
     weight: 35
     week: 6
     day: 3
+    course: true
     ---
-    """
-  end
-
-  defp file_content do
-    """
-    <%= figure(%{src: "//module/images/css_flexbox_axes.png", class: "rounded mx-auto d-block", caption: "CSS Flexbox Axes", width: "600px", height: "auto", alt: "CSS Flexbox axes description"}) %>
-
-    [Turma 3 - Início jan/2020](/content/module/subject/sd-cohort-3)
-
-    [Turma 5 - Início jan/2020](#sd-cohort-5)
     """
   end
 end
