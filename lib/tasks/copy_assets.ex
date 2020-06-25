d1150d0b1ad663e558dcf8956d73882a2de1e02d
Norberto Oliveira Junior <norbejunior@gmail.com>
2020-06-25 14:02:00 -0300

    Refatora HugoToEExConvert.Images
    Este módulo precisa copiar não só arquivos imagens
    
    * Renomeado o módulos para HugoToEExConvert.Assets e HugoToEExConvert.AssetsTest
    * Renomeado módulo da mix task para HugoToEExConvert.CopyAssets
    * Renomeado comando `mix hugo_to_converter.copy_images` para `mix hugo_to_converter.copy_assets`
    * Renomeado algumas variáveis
    * Adicionado mais testes para diferentes arquivos

--- lib/tasks/copy_assets.ex
+++ lib/tasks/copy_assets.ex
@@ -1,28 +1,28 @@
-defmodule Mix.Tasks.HugoToEex.CopyImages do
+defmodule Mix.Tasks.HugoToEex.CopyAssets do
   use Mix.Task
 
-  @shortdoc "Copy images from course content"
+  @shortdoc "Copy assets from course"
   @moduledoc ~S"""
-    Copy content images from a folder #{source}/convert to #{source}/converted
+    Copy assets from a folder #{source}/convert to #{source}/converted
     keeping the relative path
 
     # Usage
     ```
-    mix hugo_to_eex.copy_images
+    mix hugo_to_eex.copy_assets
     ```
 
     You  can also pass as argument the source path where the files are
 
     ```
-    mix hugo_to_eex.copy_images --source tmp/convert
+    mix hugo_to_eex.copy_assets --source tmp/convert
     ```
   """
-  def run([]), do: HugoToEExConverter.Images.copy()
+  def run([]), do: HugoToEExConverter.Assets.copy()
 
   def run(args) do
     case OptionParser.parse(args, strict: [source: :string]) do
       {[source: source], _, _} ->
-        HugoToEExConverter.Images.copy(source)
+        HugoToEExConverter.Assets.copy(source)
 
       {_, _, [{option, _}]} ->
         IO.puts("\nInvalid param #{option}. Did you mean --source?")
